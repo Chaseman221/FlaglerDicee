3405ad4d1ab404ca4baba0efa7b257e001707686
Ashton <aceman1043@gmail.com>
2025-02-20 19:07:35 -0500

    Part 2

--- FlaglerDicee/ViewController.swift
+++ FlaglerDicee/ViewController.swift
@@ -2,39 +2,69 @@
 //  ViewController.swift
 //  Flagler Dicee
 //
+// The enhancements are the new abilities of the Unroll button to hide when not usable, the background music and the roll sound effect, and the animated logo
 
 import UIKit
+//Import audio kit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    //setup audio
+    var backgroundAudioPlayer: AVAudioPlayer?
+    var diceAudioPlayer: AVAudioPlayer?
+    
+    @IBOutlet weak var diceeLogoView: UIImageView!
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
+    @IBOutlet weak var unrollButtonUI: UIButton!
     
     var leftDiceNumber = 0
     var rightDiceNumber = 5
+    var previousLeftDiceNumber = 0
+    var previousRightDiceNumber = 0
+    var diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    }
-
-    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        unrollButtonUI.isHidden = true
         
-        leftDiceNumber = leftDiceNumber + 1
-        rightDiceNumber = rightDiceNumber - 1
+        //play background music
+        if let url = Bundle.main.url(forResource: "backgroundMusic", withExtension: "mp3") {
+            backgroundAudioPlayer = try? AVAudioPlayer(contentsOf: url)
+            backgroundAudioPlayer?.numberOfLoops = -1
+            backgroundAudioPlayer?.play()
+        }
+    }
+    
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        //Controls the storage for the unroll function
+        previousLeftDiceNumber = leftDiceNumber
+        previousRightDiceNumber = rightDiceNumber
+        unrollButtonUI.isHidden = false
+        
+        leftDiceNumber = Int.random(in: 0...5)
+        rightDiceNumber = Int.random(in: 0...5)
         
         // let _____ #imageLiteral(
-        diceImageView1.image = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")][leftDiceNumber]
-        diceImageView2.image = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")][rightDiceNumber]
+        diceImageView1.image = diceArray[leftDiceNumber]
+        diceImageView2.image = diceArray[rightDiceNumber]
+        
+        PlaySound()
+        AnimateLogo()
+        
     }
     
     @IBAction func unrollButtonPressed(_ sender: UIButton) {
         
-        leftDiceNumber = leftDiceNumber - 1
-        rightDiceNumber = rightDiceNumber + 1
+        diceImageView1.image = diceArray[previousLeftDiceNumber]
+        diceImageView2.image = diceArray[previousRightDiceNumber]
+        sender.isHidden = true
         
-        diceImageView1.image = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")][leftDiceNumber]
-        diceImageView2.image = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")][rightDiceNumber]
+        PlaySound()
+        AnimateLogo()
+        
     }
     
     @IBAction func snakeButtonPressed(_ sender: UIButton) {
@@ -42,8 +72,35 @@ class ViewController: UIViewController {
         leftDiceNumber = 0
         rightDiceNumber = 0
         
-        diceImageView1.image = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")][leftDiceNumber]
-        diceImageView2.image = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")][rightDiceNumber]
+        diceImageView1.image = diceArray[leftDiceNumber]
+        diceImageView2.image = diceArray[rightDiceNumber]
+        
+        PlaySound( )
+        AnimateLogo()
     }
+    
+    //Create reusable function to play dice sounds
+    public func PlaySound() {
+        
+        if let url = Bundle.main.url(forResource: "diceThrow", withExtension: "mp3") {
+            diceAudioPlayer = try? AVAudioPlayer(contentsOf: url)
+            diceAudioPlayer?.play()
+        }
+    }
+    
+    
+    //Waits work synchronously not sequentially
+    public func AnimateLogo() {
+        diceeLogoView.image = #imageLiteral(resourceName: "diceeLogoEmptyFrame")
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+            self.diceeLogoView.image = #imageLiteral(resourceName: "DiceeLogo")
+        }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+            self.diceeLogoView.image = #imageLiteral(resourceName: "diceeLogoEmptyFrame")
+        }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
+            self.diceeLogoView.image = #imageLiteral(resourceName: "DiceeLogo")
+        }
+    }
+    
 }
-
